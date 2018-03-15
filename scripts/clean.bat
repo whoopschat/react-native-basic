e083bb7c9ce0fc4d62694b788f2bda54d5015786
liangmayong <liangmayong@surevideo.me>
2018-03-15 18:21:14 +0800

    fix

---
+++ scripts/clean.bat
@@ -0,0 +1,44 @@
+@echo off
+
+:: clean build file
+
+if exist ../ios/build (
+    rd /q /s "../ios/build"
+)
+if exist ../android/app/build (
+    rd /q /s "../android/app/build"
+)
+
+:: clean res file
+
+if exist "..\android\app\src\main\res\drawable-mdpi\src_res_*.*" (
+    del /q /s "..\android\app\src\main\res\drawable-mdpi\src_res_*.*"
+)
+if exist "..\android\app\src\main\res\drawable-mdpi\node_modules_*.*" (
+    del /q /s "..\android\app\src\main\res\drawable-mdpi\node_modules_*.*"
+)
+if exist "..\android\app\src\main\res\drawable-hdpi\src_res_*.*" (
+    del /q /s "..\android\app\src\main\res\drawable-hdpi\src_res_*.*"
+)
+if exist "..\android\app\src\main\res\drawable-hdpi\node_modules_*.*" (
+    del /q /s "..\android\app\src\main\res\drawable-hdpi\node_modules_*.*"
+)
+if exist "..\android\app\src\main\res\drawable-xhdpi\src_res_*.*" (
+    del /q /s "..\android\app\src\main\res\drawable-xhdpi\src_res_*.*"
+)
+if exist "..\android\app\src\main\res\drawable-xhdpi\node_modules_*.*" (
+    del /q /s "..\android\app\src\main\res\drawable-xhdpi\node_modules_*.*"
+)
+if exist "..\android\app\src\main\res\drawable-xxhdpi\src_res_*.*" (
+    del /q /s "..\android\app\src\main\res\drawable-xxhdpi\src_res_*.*"
+)
+if exist "..\android\app\src\main\res\drawable-xxhdpi\node_modules_*.*" (
+    del /q /s "..\android\app\src\main\res\drawable-xxhdpi\node_modules_*.*"
+)
+if exist "..\android\app\src\main\res\drawable-xxxhdpi\src_res_*.*" (
+    del /q /s "..\android\app\src\main\res\drawable-xxxhdpi\src_res_*.*"
+)
+if exist "..\android\app\src\main\res\drawable-xxxhdpi\node_modules_*.*" (
+    del /q /s "..\android\app\src\main\res\drawable-xxxhdpi\node_modules_*.*"
+)
+echo Done
