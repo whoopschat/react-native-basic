e305691da4758282a27ede54eb11c44735cfe8cb
liangmayong <liangmayong@surevideo.me>
2018-03-16 19:50:34 +0800

    fix bugs

--- src/common/vendors/bridge.js
+++ src/common/vendors/bridge.js
@@ -7,71 +7,83 @@ const bridgeModuleEmitEvent = 'RNBridgeEvent';
 const bridgeNativeHandleEvent = 'handleJsEvent';
 const bridgeReadyEvent = 'RNBridgeReady';
 
-const bridge = NativeModules[bridgeModuleName];
+const createBridge = () => {
+  if (NativeModules.hasOwnProperty(bridgeModuleName)) {
+    const bridge = NativeModules[bridgeModuleName];
 
-// event
-const handlersByType = new Map();
+    // event
+    const handlersByType = new Map();
 
-// send
-const send = (type, data) => {
-  const payload = data !== undefined ? data : {};
-  if (bridge.hasOwnProperty(bridgeNativeHandleEvent)) {
-    const handle = bridge[bridgeNativeHandleEvent];
-    if (typeof handle === 'function') {
-      return handle(type, payload)
-    }
+    // send
+    const send = (type, data) => {
+      const payload = data !== undefined ? data : {};
+      if (bridge.hasOwnProperty(bridgeNativeHandleEvent)) {
+        const handle = bridge[bridgeNativeHandleEvent];
+        if (typeof handle === 'function') {
+          return handle(type, payload)
+        }
+      }
+      console.error(bridgeModuleName + " can't find the " + bridgeNativeHandleEvent + " method");
+    };
+
+    const eventEmitter = Platform.OS === 'ios'
+      ? /*ios*/ new NativeEventEmitter(bridge)
+      : /*android*/ DeviceEventEmitter;
+
+    eventEmitter.addListener(bridgeModuleEmitEvent, (body) => {
+      const {type, data} = body;
+      const handlers = handlersByType.get(type);
+      if (!handlers) {
+        return;
+      }
+      handlers.forEach(handler => handler(data));
+    });
+
+    const createRemover = (type, handler) => {
+      const remove = () => {
+        const handlers = handlersByType.get(type);
+        if (!handlers) {
+          return;
+        }
+        const index = handlers.indexOf(handler);
+        if (index === -1) {
+          return;
+        }
+        handlers.splice(index, 1);
+      };
+      return {remove};
+    };
+
+    const listener = (type, handler) => {
+      if (!handlersByType.has(type)) {
+        handlersByType.set(type, []);
+      }
+      const remover = createRemover(type, handler);
+      const handlers = handlersByType.get(type);
+      if (handlers.includes(handler)) {
+        return remover;
+      }
+      handlers.push(handler);
+      return remover;
+    };
+
+    const ready = () => {
+      return send(bridgeReadyEvent);
+    };
+
+    return {
+      send,
+      listener,
+      ready
+    };
+  } else {
+    return {
+      send: () => Promise.resolve({}),
+      listener: () => Promise.resolve({}),
+      ready: () => Promise.resolve({})
+    };
   }
-  console.error(bridgeModuleName + " can't find the " + bridgeNativeHandleEvent + " method");
 };
 
-const eventEmitter = Platform.OS === 'ios'
-  ? /*ios*/ new NativeEventEmitter(bridge)
-  : /*android*/ DeviceEventEmitter;
-
-eventEmitter.addListener(bridgeModuleEmitEvent, (body) => {
-  const {type, data} = body;
-  const handlers = handlersByType.get(type);
-  if (!handlers) {
-    return;
-  }
-  handlers.forEach(handler => handler(data));
-});
-
-const createRemover = (type, handler) => {
-  const remove = () => {
-    const handlers = handlersByType.get(type);
-    if (!handlers) {
-      return;
-    }
-    const index = handlers.indexOf(handler);
-    if (index === -1) {
-      return;
-    }
-    handlers.splice(index, 1);
-  };
-  return {remove};
-};
-
-const listener = (type, handler) => {
-  if (!handlersByType.has(type)) {
-    handlersByType.set(type, []);
-  }
-  const remover = createRemover(type, handler);
-  const handlers = handlersByType.get(type);
-  if (handlers.includes(handler)) {
-    return remover;
-  }
-  handlers.push(handler);
-  return remover;
-};
-
-const ready = () => {
-  return send(bridgeReadyEvent);
-};
-
-export default {
-  send,
-  listener,
-  ready
-};
+export default createBridge();
 
