fe9b4d6ae12fd28ff25c471e04be66a3f05bc90f
liangmayong <liangmayong@surevideo.me>
2018-03-16 19:23:07 +0800

    add bundle js

---
+++ scripts/bundle-android.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const fs = require('fs');
+const rm = require('rimraf');
+const path = require('path');
+const chalk = require('chalk');
+const exec = require('child_process').exec;
+
+const buildPath = "build/android";
+const cmd = 'react-native bundle' +
+  ' --platform android' +
+  ' --dev false' +
+  ' --entry-file index.js' +
+  ' --bundle-output ' + buildPath + '/index.android.bundle' +
+  ' --assets-dest ' + buildPath +
+  ' --reset-cache';
+
+console.log('  building for android bundle....\n')
+rm(buildPath, function () {
+  createFolder(buildPath, () => {
+    exec(cmd, function (error, stdout, stderr) {
+      if (error != null) {
+        console.log(chalk.red(error));
+      } else {
+        console.log(`${stdout}`);
+        if (stderr != null) {
+          console.log(chalk.red(stderr));
+        }
+        console.log(chalk.cyan('  Build complete.\n'))
+      }
+    });
+  });
+});
+
+
+/////////////////////////////
+/////// createFolder
+/////////////////////////////
+
+function createFolder(dirname, callback) {
+  fs.exists(dirname, function (exists) {
+    if (exists) {
+      callback();
+    } else {
+      createFolder(path.dirname(dirname), function () {
+        fs.mkdir(dirname, callback);
+      });
+    }
+  });
+}
+
